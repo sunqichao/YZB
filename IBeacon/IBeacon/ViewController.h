f5185909189fa5cf6960563807824bbfd40c16d2
sunqichao <macbook@MacBookdeMacBook-Pro.local>
2014-06-03 18:06:26 +0800

    init YZB
    add init project

---
+++ IBeacon/IBeacon/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IBeacon
+//
+//  Created by Tony on 14-6-2.
+//  Copyright (c) 2014年 YZB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ IBeacon/IBeacon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IBeacon
+//
+//  Created by Tony on 14-6-2.
+//  Copyright (c) 2014年 YZB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
